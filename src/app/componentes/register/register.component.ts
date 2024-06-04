a07bb8d8978432495aae2c68fbc5e939840f7e0e
cr7kawai <rojassanchezo63@gmail.com>
2024-06-04 13:31:05 -0600

    Creación del registro y validación de datos

--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { passwordMatchValidator } from '../../shared/password-match.directives';
+import { AuthService } from '../../servicios/auth.service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +12,33 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  registerForm = this.fb.group({
+    fullName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$')]],
+    confirmPassword: ['', Validators.required]
+  },{
+    validators: passwordMatchValidator
+  });
+
+  constructor(
+    private fb: FormBuilder, 
+    private authService: AuthService,
+    private messageService: MessageService,
+    private router: Router
+  ){}
+
+  get fullName(){
+    return this.registerForm.controls['fullName'];
+  }
+
+  get email(){
+    return this.registerForm.controls['email'];
+  }
+  get password(){
+    return this.registerForm.controls['password'];
+  }
+  get confirmPassword(){
+    return this.registerForm.controls['confirmPassword'];
+  }
 }
